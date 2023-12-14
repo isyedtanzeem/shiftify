b53a561bdb4cf0842818ed5ad93c4c44bb71d426
Syed Tanzeem <syed.tanzeem@magnificenztech.com>
2023-12-14 21:57:10 +0530

    root

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Routes, Route, NavLink , Link} from 'react-router-dom';
 import Menu from './Pages/Menu';
 import './App.css';
-import HtmQuotation from './Pages/Quotation';
-import HtmInvoice from './Pages/Invoice';
-import HtmLr from './Pages/Lr';
+import Quotation from './Pages/Quotation';
+import Invoice from './Pages/Invoice';
+import Lr from './Pages/Lr';
 
 
 class App extends Component {
@@ -16,9 +16,9 @@ class App extends Component {
             <Routes basename="/Quotationmaker">
               <Route path="/Shiftify" element={<Menu />} />
               <Route path="/" element={<Menu />} />
-              <Route exact path="/HtmQoutation" element={<HtmQuotation />} />
-              <Route exact path="/HtmInvoice" element={<HtmInvoice />} />
-              <Route exact path="/HtmLr" element={<HtmLr />} />
+              <Route exact path="/Qoutation" element={<Quotation />} />
+              <Route exact path="/Invoice" element={<Invoice />} />
+              <Route exact path="/Lr" element={<Lr />} />
             </Routes>
       
         </Router>
