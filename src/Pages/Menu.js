31a61cea7c6a917f87685da8f7209572819df6c7
Syed Tanzeem <syed.tanzeem@magnificenztech.com>
2023-12-14 23:07:50 +0530

    logo in main

--- src/Pages/Menu.js
+++ src/Pages/Menu.js
@@ -2,10 +2,8 @@ import React, { useState } from "react";
 import "./Form.css";
 import jsPDF from "jspdf";
 
-import SupportLogo from "./support-logo.png";
+import logo from "./Images/Logo.png";
 
-import resix from "./Images/resix.png";
-import Signature from "./Images/signature.png";
 import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
 
 
@@ -14,7 +12,7 @@ const Menu = () => {
   return (
     <div className="form-container">
 	{/* <h4><Link to="/">Go to Home</Link></h4> */}
-      <h2> Shiftify Packers and Movers</h2>
+      <img src={logo} className="imgWidth"></img>
       <div className="button-container">  
         <Link to='/Qoutation' className="htm-button"> Quotation</Link>
        </div>
