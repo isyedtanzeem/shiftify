8958a86c79a8f1c6424f627e55a4764fe89e3b91
Syed Tanzeem <syed.tanzeem@magnificenztech.com>
2023-12-14 21:59:29 +0530

    bugs

--- src/Pages/Menu.js
+++ src/Pages/Menu.js
@@ -14,16 +14,16 @@ const Menu = () => {
   return (
     <div className="form-container">
 	{/* <h4><Link to="/">Go to Home</Link></h4> */}
-      <h2> -----Quotation,Invoice and LR-----</h2>
+      <h2> Shiftify Packers and Movers</h2>
       <div className="button-container">  
-        <Link to='/HtmQoutation' className="htm-button"> Quotation</Link>
+        <Link to='/Qoutation' className="htm-button"> Quotation</Link>
        </div>
       <div className="button-container">  
-   <Link to='/HtmInvoice' className="htm-button">Invoice</Link>
+   <Link to='/Invoice' className="htm-button">Invoice</Link>
        </div>
       <div className="button-container">  
        
-   <Link to='/HtmLr' className="htm-button"> LR</Link>
+   <Link to='/Lr' className="htm-button"> LR</Link>
        </div>
        
       
