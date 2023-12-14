e12a951b94895af3c81f16b096c29e008a517269
Syed Tanzeem <syed.tanzeem@magnificenztech.com>
2023-12-14 22:47:01 +0530

    inovice 1

--- src/Pages/Invoice.js
+++ src/Pages/Invoice.js
@@ -2,14 +2,11 @@ import React, { useState } from "react";
 import "./Form.css";
 import jsPDF from "jspdf";
 
-import Logo from "./Images/Logo.png"; // Import your logo image
-import SupportLogo from "./support-logo.png";
+import Head from "./Images/ShiftifyInvoice.png";
+import seal from "./Images/seal.png";
 
-import resix from "./Images/resix.png";
-import Signature from "./Images/signature.png";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-const BocInvoice = () => {
+const Invoice = () => {
   const [formData, setFormData] = useState({
     transportCharges: "",
     from: "",
@@ -72,42 +69,11 @@ const BocInvoice = () => {
   const generatePDF = (count) => {
     const pdf = new jsPDF();
     pdf.setFont("helvetica", "normal");
-    pdf.setFontSize(14);
+   
 
-    pdf.addImage(Logo, "PNG", 15, 5, 80, 20);
-    pdf.setFontSize(8);
-    pdf.text(
-      12,
-      30,
-      "No. 320,10th cross NGR Layout, Masjid Road,Roopena Agrahara, Bangalore - 560068"
-    );
-    pdf.text(
-      12,
-      34,
-      "E-Mail :htmpackersandmovers@gmail.com             Web : www.htmmovers.in"
-    );
+    pdf.addImage(Head, "PNG", 12, 6, 185, 34);
+
     pdf.setFontSize(10);
-    pdf.setFont(undefined, "bold");
-
-    pdf.text(130, 18, "FRIGHT BILL");
-
-    pdf.setFillColor(0,63,171);
-
-    // Add a filled rectangular box to the PDF
-    pdf.rect(130, 22, 26, 10, "F"); // 'F' stands for 'Fill'
-
-    // Set text color to white
-    pdf.setTextColor(255, 255, 255); // White color
-    pdf.text(`CUSTOMER`, 133, 26.5);
-    pdf.text(`HELPLINE`, 134.5, 30.5);
-
-    pdf.setTextColor(0, 0, 0);
-    pdf.setFontSize(10);
-    pdf.setFont(undefined, "none");
-    pdf.text(156, 26, " 72042 08209");
-    pdf.text(156, 30, " 97412 42669");
-    pdf.addImage(SupportLogo, "PNG", 176, 21, 15, 10);
-
     //input
     pdf.rect(12, 41, 95, 7);
     pdf.text(`NAME: ${formData.name}`, 13, 45, { align: "left" });
@@ -117,37 +83,37 @@ const BocInvoice = () => {
     pdf.text(`ADDRESS: ${formData.address}`, 13, 59.5, { align: "left" });
 
     const currentDate = new Date().toLocaleDateString();
-    pdf.rect(107, 41, 83, 7);
+    pdf.rect(107, 41, 90, 7);
     pdf.text(`DATE: ${currentDate}`, 108, 45.4);
     pdf.text(`BILL NO: ${count}`, 108, 52.5, { align: "left" });
 
-    pdf.rect(107, 48, 83, 7);
-    pdf.rect(107, 55, 83, 7);
+    pdf.rect(107, 48, 90, 7);
+    pdf.rect(107, 55, 90, 7);
     pdf.text(`FROM: ${formData.from}`, 108, 59.5);
 
-    pdf.rect(107, 62, 83, 7);
+    pdf.rect(107, 62, 90, 7);
     pdf.text(`TO: ${formData.toLocation}`, 108, 65.5);
 
     //end of first layer
 
     pdf.setFontSize(16);
-    pdf.setFillColor(0,63,171);
-    pdf.rect(12, 82, 180, 10, "F");
+    pdf.setFillColor(0, 151, 178);
+    pdf.rect(12, 82, 185, 10, "F");
 
     pdf.setDrawColor(0, 0, 0);
     pdf.rect(12, 82, 125, 10);
-    pdf.rect(137, 82, 55, 10);
+    pdf.rect(137, 82, 60, 10);
 
     pdf.setFont(undefined, "bold");
     pdf.setTextColor(255, 255, 255);
-    pdf.text(`Particulars`, 42, 88);
+    pdf.text(`Particulars`, 56, 88);
     pdf.text(`Amount`, 154, 88);
 
     pdf.setTextColor(0, 0, 0);
     pdf.setFont(undefined, "none");
     //transportation charges
     pdf.rect(12, 92, 125, 10);
-    pdf.rect(137, 92, 55, 10);
+    pdf.rect(137, 92, 60, 10);
     pdf.text(`Transportation Charges`, 42, 98);
     pdf.text(`${formData.transportCharges}`, 154, 98);
 
@@ -216,44 +182,43 @@ const BocInvoice = () => {
     pdf.setFont(undefined, "none");
 
     pdf.rect(12, 102, 125, 10);
-    pdf.rect(137, 102, 55, 10);
+    pdf.rect(137, 102, 60, 10);
     pdf.rect(12, 112, 125, 10);
-    pdf.rect(137, 112, 55, 10);
+    pdf.rect(137, 112, 60, 10);
     pdf.rect(12, 122, 125, 10);
-    pdf.rect(137, 122, 55, 10);
+    pdf.rect(137, 122, 60, 10);
     pdf.rect(12, 132, 125, 10);
-    pdf.rect(137, 132, 55, 10);
+    pdf.rect(137, 132, 60, 10);
     pdf.rect(12, 142, 125, 10);
-    pdf.rect(137, 142, 55, 10);
+    pdf.rect(137, 142, 60, 10);
     pdf.rect(12, 152, 125, 10);
-    pdf.rect(137, 152, 55, 10);
+    pdf.rect(137, 152, 60, 10);
     pdf.rect(12, 162, 125, 10);
-    pdf.rect(137, 162, 55, 10);
+    pdf.rect(137, 162, 60, 10);
 
     pdf.rect(108, 172, 29, 8);
-    pdf.rect(137, 172, 55, 8);
+    pdf.rect(137, 172, 60, 8);
 
     pdf.rect(108, 180, 29, 10);
-    pdf.rect(137, 180, 55, 10);
+    pdf.rect(137, 180, 60, 10);
 
     pdf.rect(108, 190, 29, 10);
-    pdf.rect(137, 190, 55, 10);
+    pdf.rect(137, 190, 60, 10);
 
-    pdf.text(`NOTE/DESCRIPTION`, 12, 210);
-    pdf.rect(12, 213, 94, 15);
+    pdf.text(`NOTE/DESCRIPTION`, 14, 180);
+    pdf.rect(12, 172, 96, 11);
+    pdf.rect(12, 183, 96, 17);
     pdf.setFontSize(14);
     pdf.text(`${formData.description}`, 13, 222);
 
     pdf.setFontSize(16);
     pdf.setTextColor(0,63,171);
 
-    pdf.text(`For HTM PACKERS AND MOVERS`, 106, 250);
-
-    pdf.setFontSize(14);
+    pdf.text(`For SHIFTIFY PACKERS AND MOVERS`, 100, 215);
+    pdf.addImage(seal, "PNG", 135, 215, 42, 42);
+    pdf.setFontSize(10);
     pdf.setTextColor(0, 0, 0);
-    pdf.addImage(Signature, "PNG", 128, 264, 28, 10);
-
-    pdf.text(`Accountant/Manager`, 122, 280);
+    pdf.text(`This is digitally generated invoice`, 18, 282);
 
 
 
@@ -261,7 +226,7 @@ const BocInvoice = () => {
     const sanitizedName = formData.name.replace(/[^a-zA-Z0-9]/g, "");
     const sanitizedMobile = formData.mobile.replace(/[^0-9]/g, "");
 
-    const pdfName = `Htm_Invoice_${sanitizedName}.pdf`;
+    const pdfName = `Shiftify_Invoice_${sanitizedName}.pdf`;
 
     pdf.save(pdfName);
   };
@@ -419,4 +384,4 @@ const BocInvoice = () => {
   );
 };
 
-export default BocInvoice;
+export default Invoice;
